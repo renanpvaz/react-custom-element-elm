acdd9ab0611df3afe18985d5e360908292a7476c
renanpvaz <renan.piresvz@gmail.com>
2018-06-28 15:18:07 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,27 +1,2 @@
-# elm-google-maps
-
-Google Maps in Elm 0.18!
-
-## Prerequisites
-
-Must have the Elm 0.18 installed already.
-
-```
-$ npm install -g elm-live
-$ npm install -g bower
-```
-
-## Running the Example
-
-```shell
-$ git clone git@github.com:rtfeldman/elm-google-maps.git
-$ cd elm-google-maps
-$ bower install
-$ elm-live src/Main.elm --output=app.js --open -- --debug
-```
-
-## Credit
-
-This began as a fork of @fredcy's [`elm-polymer-calendar`](https://github.com/fredcy/elm-polymer-calendar) which in turn
-was a direct copy of a gist by Peter Damoc (@pdamoc) and relates to
-[a discussion on the elm-discuss list](https://groups.google.com/forum/#!topic/elm-discuss/8Q2xwRh6UYc)
+```npm i```
+```npm start```
--- src/Main.elm
+++ src/Main.elm
@@ -36,93 +36,14 @@ init =
 
 
 type Msg
-    = SetLatitude Float
-    | SetLongitude Float
-    | SetLatLong Float Float
+    = DomNoOp
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    case msg of
-        SetLatitude latitude ->
-            { model | latitude = latitude } ! []
-
-        SetLongitude longitude ->
-            { model | longitude = longitude } ! []
-
-        SetLatLong latitude longitude ->
-            { model | latitude = latitude, longitude = longitude } ! []
-
-
-googleMap : List (Attribute a) -> List (Html a) -> Html a
-googleMap =
-    Html.node "google-map"
+    ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div []
-            [ label [] [ text "Latitude" ]
-            , input
-                [ type_ "range"
-                , attribute "min" "-1800000"
-                , attribute "max" "1800000"
-                , defaultValue (toString model.latitude)
-                , onChange SetLatitude
-                ]
-                []
-            , span [] [ text (toString model.latitude) ]
-            ]
-        , div []
-            [ label [] [ text "Longitude" ]
-            , input
-                [ type_ "range"
-                , attribute "min" "-1800000"
-                , attribute "max" "1800000"
-                , defaultValue (toString model.longitude)
-                , onChange SetLongitude
-                ]
-                []
-            , span [] [ text (toString model.longitude) ]
-            ]
-        , img
-            [ class "elm-logo"
-            , src "http://package.elm-lang.org/assets/elm_logo.svg"
-            ]
-            []
-        , googleMap
-            [ attribute "latitude" (toString model.latitude)
-            , attribute "longitude" (toString model.longitude)
-            , attribute "drag-events" "true"
-            , recordLatLongOnDrag
-            ]
-            []
-        ]
-
-
-recordLatLongOnDrag : Attribute Msg
-recordLatLongOnDrag =
-    on "google-map-drag" <|
-        map2 SetLatLong
-            (at [ "target", "latitude" ] float)
-            (at [ "target", "longitude" ] float)
-
-
-onChange : (Float -> Msg) -> Attribute Msg
-onChange toMsg =
-    Decode.string
-        |> Decode.andThen decodeLatLong
-        |> Decode.at [ "target", "value" ]
-        |> Decode.map toMsg
-        |> on "change"
-
-
-decodeLatLong : String -> Decoder Float
-decodeLatLong str =
-    case Decode.decodeString Decode.float str of
-        Ok num ->
-            Decode.succeed (num / 10000)
-
-        Err err ->
-            Decode.fail err
+    Html.node "leaflet-map" [] []
